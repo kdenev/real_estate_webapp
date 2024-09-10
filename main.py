b5ffa1b3fc6f986302253d5d8953155405a15b5f
kdenev <kristiyan.denev@gmail.com>
2024-09-10 22:16:14 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from datetime import date
+from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from dotenv import load_dotenv
+# Optional: add contact me email functionality (Day 60)
+# import smtplib
+from flask_babel import Babel
+from flask_babel import _
+
+# Load environment variables
+load_dotenv()
+
+
+def get_locale():
+    print(os.environ.get('LANGUAGES'))
+    return request.accept_languages.best_match(os.environ.get('LANGUAGES'))
+
+app = Flask(__name__)
+babel = Babel(app, locale_selector=get_locale)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+ckeditor = CKEditor(app)
+Bootstrap5(app)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
